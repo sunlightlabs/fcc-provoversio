639a8f96c7be9304b42dbbe061baea0e78aba561
Daniel Cloud <dcloud@sunlightfoundation.com>
2013-08-06 15:34:27 -0400

    Show xml payload for example data.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,21 @@
 require.config({ baseUrl: 'js/lib',
-    paths: {
-        templates:'../tpl',
-        data: '../data',
-        jquery: 'jquery-1.10.2.min',
-        sfapp: '../../sfapp'
-    },
-    shim: {
-            "underscore-min": {
-              exports: '_'
-            }
-        }
+    paths: { templates:'../tpl', data: '../data', jquery: 'jquery-1.10.2.min', sfapp: '../../sfapp' },
+    shim: { "underscore-min": { exports: '_' } }
 });
 
-require(['jquery', 'underscore-min', 'text', 'handlebars', 'text!templates/form.handlebars', 'data/typeaheads', 'modernizr.min',
-        'sfapp/js/bootstrap.min', 'sfapp/js/sfapp'],
-function($, _, text, handlebars, form_src, typeaheads) {
-    var fec_names, form_tpl, data_uri, subject_selector, candidate_check_selector;
+require(['jquery', 'underscore-min', 'text', 'handlebars',
+        'text!templates/form.handlebars','text!templates/payload.xml.handlebars', 'data/typeaheads',
+        'modernizr.min', 'sfapp/js/bootstrap.min', 'sfapp/js/sfapp'],
+function($, _, text, handlebars, form_src, xml_src, typeaheads) {
+    var fec_names, form_tpl, xml_tpl,
+    data_uri, subject_selector, candidate_check_selector;
 
-    data_uri = 'js/data/example_data.json';
-    subject_selector = 'select[name="advertisement_subject"]';
+    subject_selector = 'select[name=advertisement_subject]';
     candidate_check_selector = 'input[name=by_candidate]';
+    // Data and tpl setup
+    data_uri = 'js/data/example_data.json';
     form_tpl = Handlebars.compile(form_src);
+    xml_tpl = Handlebars.compile(xml_src);
 
     function trigger_form_changes () {
         $(subject_selector).change();
@@ -44,71 +39,87 @@ function($, _, text, handlebars, form_src, typeaheads) {
         });
     }
 
+    function processPlaceholders () {
+        /*
+            Enable placeholders on browsers without native support
+        */
+        if (!Modernizr.input.placeholder) {
+            var formpl = '#fcc_form [placeholder]';
+            $(formpl).each(function(event) {
+                if ($(this).val() === '') // if field is empty
+                {
+                    $(this).addClass('muted');
+                    $(this).val( $(this).attr('placeholder') );
+                }
+                else {
+                    $(this).removeClass('muted');
+                }
+            });
+            $(document).on('focus', formpl, function(event) {
+                event.preventDefault();
+                if ($(this).val() == $(this).attr('placeholder'))
+                {
+                    $(this).val('');
+                    $(this).removeClass('muted');
+                }
+            });
+            $(document).on('blur', formpl, function(event) {
+                event.preventDefault();
+                if ($(this).val() === '' || $(this).val() == $(this).attr('placeholder'))
+                {
+                    $(this).addClass('muted');
+                    $(this).val($(this).attr('placeholder'));
+                }
+                else {
+                    $(this).removeClass('muted');
+                }
+            });
+            $(document).on('submit', 'form', function(event) {
+                event.preventDefault();
+                $(this).find('[placeholder]').each(function()
+                {
+                    if ($(this).val() == $(this).attr('placeholder'))
+                    {
+                        $(this).val('');
+                    }
+                });
+            });
+
+        }
+    }
+
     $(document).ready(function($) {
         // Display form
-        $('#app').html(form_tpl);
+        $('#app-form').html(form_tpl);
         trigger_form_changes();
         attach_typeaheads();
+        processPlaceholders();
+        fec_names = _.keys(typeaheads.fec);
 
         // Button for loading example data
         $(document).on('click', '#load_eg', function(event) {
             event.preventDefault();
-            $('#app').fadeOut('slow', function() {
+            $('#app-form').fadeOut('slow', function() {
                 $.getJSON(data_uri, function(json, textStatus, jqXHR) {
                     var compiled = form_tpl(json);
-                    $('#app').html(compiled);
-                    $('#app').fadeIn('slow', function() {
+                    $('#app-form').html(compiled);
+                    $('#app-form').fadeIn('slow', function() {
                         $('#advertisement_subject option').filter('[value='+ json.advertisement.subject + ']').prop('selected', true);
                         trigger_form_changes();
+                        attach_typeaheads();
+                        processPlaceholders();
                     });
+                    var comp_xml = xml_tpl(json);
+                    var pre = $('<pre>');
+                    pre.html(comp_xml);
+                    $('#app-data').html(pre);
                 });
 
             });
         });
 
-        fec_names = _.keys(typeaheads.fec);
         /*
-            Enable placeholders on browsers without native support
-        */
-        if (!Modernizr.input.placeholder) {
-            $(this).find('[placeholder]').each(function()
-            {
-                if ($(this).val() === '') // if field is empty
-                {
-                    $(this).val( $(this).attr('placeholder') );
-                }
-            });
-            $('[placeholder]').focus(function()
-                {
-                    if ($(this).val() == $(this).attr('placeholder'))
-                    {
-                        $(this).val('');
-                        $(this).removeClass('placeholder');
-                    }
-                }).blur(function()
-                {
-                    if ($(this).val() === '' || $(this).val() == $(this).attr('placeholder'))
-                    {
-                        $(this).val($(this).attr('placeholder'));
-                        $(this).addClass('placeholder');
-                    }
-                });
-
-                // remove placeholders on submit
-                $('[placeholder]').closest('form').submit(function()
-                {
-                    $(this).find('[placeholder]').each(function()
-                    {
-                        if ($(this).val() == $(this).attr('placeholder'))
-                        {
-                            $(this).val('');
-                        }
-                    });
-                });
-        }
-
-        /*
-            form#fcc_form stuff
+            form#fcc_form Events
         */
         $(document).on('change', subject_selector, function(evt) {
             var field_val = $(this).val();
@@ -119,7 +130,7 @@ function($, _, text, handlebars, form_src, typeaheads) {
                 el.show();
             }
             else {
-                inputs.attr('required', 'required');
+                inputs.removeAttr('required');
                 el.hide();
             }
         });
@@ -132,7 +143,7 @@ function($, _, text, handlebars, form_src, typeaheads) {
                 el.show();
             }
             else {
-                inputs.attr('required', 'required');
+                inputs.removeAttr('required');
                 el.hide();
             }
         });
@@ -141,6 +152,20 @@ function($, _, text, handlebars, form_src, typeaheads) {
             event.preventDefault();
             console.log("Submit form");
         });
+
+        $(document).on('click', '#fakereset', function(event) {
+            event.preventDefault();
+            $('#app-form').fadeOut('fast', function() {
+                // var compiled = form_tpl();
+                $('#app-form').html(form_tpl);
+                $('#app-form').fadeIn('fast', function() {
+                    trigger_form_changes();
+                    attach_typeaheads();
+                    processPlaceholders();
+                });
+            });
+        });
+
 
     });
 });
